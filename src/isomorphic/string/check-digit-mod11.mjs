9c311844f536d33e08a7d1caa9a85f1c963f51c4
Rafael Hengles <rhengles@gmail.com>
2021-11-28 13:56:36 -0300

    Refactor check-digit-mod11

--- src/isomorphic/string/check-digit-mod11.mjs
+++ src/isomorphic/string/check-digit-mod11.mjs
@@ -1,9 +1,11 @@
 
-const noop = () => {}
+const noop = () => {};
+const echo = x => x;
 
 export const mod11Methods = {
 	init(state) {
 		state.sum = state.sum || 0;
+		state.charCount = state.charCount || 0;
 		const {factor} = state;
 		if (null == factor || isNaN(factor) || !isFinite(factor) || !(factor >= 2)) {
 			throw new Error(`Initial factor must be a number >= 2, got ${typeof factor} ${String(factor).substr(0, 64)}`);
@@ -18,6 +20,7 @@ export const mod11Methods = {
 		state.factorLast = factor;
 		state.sumLast = sum;
 		state.charLast = char;
+		state.charCount += 1;
 		sum += char * factor;
 		sum %= 11;
 		state.sum = sum;
@@ -25,7 +28,7 @@ export const mod11Methods = {
 		state.factor = factor < 3 ? 9 : factor - 1;
 	},
 	finalize: state => state.sumFin,
-}
+};
 
 export function calcDigitSum(state, {init, calcChar, finalize} = mod11Methods) {
 	init(state);
@@ -34,6 +37,7 @@ export function calcDigitSum(state, {init, calcChar, finalize} = mod11Methods) {
 		? fnDebugItem(state)
 		: noop;
 	const api = {
+		state,
 		addChar,
 		addText,
 		finalize: () => finalize(state),
@@ -51,24 +55,48 @@ export function calcDigitSum(state, {init, calcChar, finalize} = mod11Methods) {
 	}
 }
 
-export function calcDigitList(list, calcMethods = mod11Methods) {
-	let cdList = [];
-	for (const state of list) cdList.push(calcDigitSum(state, calcMethods));
+export function listCreateFactorCount(count, factor) {
+	const list = [];
+	for (let i = 0; i < count; i++) {
+		list.push({ factor: factor + i, digitIndex: i });
+	}
+	return list;
+}
+
+export function listFinalizeSumPreviousDigits(cdList) {
+	const finList = [];
+	for (const cd of cdList) {
+		cd.addText(finList);
+		const sumFin = cd.finalize();
+		finList.push(sumFin);
+	}
+	return finList;
+}
+
+export function calcDigitList(
+	calcMethods = mod11Methods,
+	listCreate = listCreateFactorCount,
+	listFinalize = listFinalizeSumPreviousDigits,
+	listInitState = echo,
+) {
+	let cdList;
 	const api = {
+		cdList,
+		init,
 		each,
-		setDebug,
 		addChar,
 		addText,
 		finalize,
 	};
 	return api;
-	function each(h) {
-		const count = list.length;
-		for (let i = 0; i < count; i++) h(list[i], i, list);
+	function init() {
+		const lis = listInitState instanceof Function ? listInitState : echo;
+		cdList = listCreate.apply(this, arguments).map(lis).map(state => calcDigitSum(state, calcMethods));
 		return api;
 	}
-	function setDebug(d) {
-		each(item => item.fnDebugItem = d);
+	function each(h) {
+		const count = cdList.length;
+		for (let i = 0; i < count; i++) h(cdList[i], i, cdList);
 		return api;
 	}
 	function addChar(char, index, text) {
@@ -80,58 +108,38 @@ export function calcDigitList(list, calcMethods = mod11Methods) {
 		return api;
 	}
 	function finalize() {
-		const finList = [];
-		for (const cd of cdList) {
-			cd.addText(finList);
-			const sumFin = cd.finalize();
-			finList.push(sumFin);
-		}
-		return finList;
+		return listFinalize(cdList);
 	}
 }
 
-export function initFactorCountList(factor, count) {
-	const list = [];
-	for (let i = 0; i < count; i++) {
-		list.push({ factor, digitIndex: i });
-		factor++;
-	}
-	return list;
-}
+export const fnCalcDigitMod11Lis = lis => calcDigitList(mod11Methods, listCreateFactorCount, listFinalizeSumPreviousDigits, lis);
 
-export function fnInitCalcDigitList(calcMethods = mod11Methods, initList = initFactorCountList) {
-	return function initCalcDigitList() {
-		const list = initList.apply(this, arguments);
-		return calcDigitList(list, calcMethods);
-	}
-}
+export const calcDigitCPF = d => fnCalcDigitMod11Lis(s => (s.fnDebugItem = d, s)).init(2, 10);
 
-export const initCalcDigitMod11 = fnInitCalcDigitList(mod11Methods, initFactorCountList);
-
-export const calcDigitCPF = () => initCalcDigitMod11(10, 2);
-
-export const calcDigitCNPJ = () => initCalcDigitMod11(5, 2);
+export const calcDigitCNPJ = d => fnCalcDigitMod11Lis(s => (s.fnDebugItem = d, s)).init(2, 5);
 
 export function fnFnDebugItem(name) {
-	return ({digits, factor, sum}) => {
-		console.log(name, digits, factor, sum);
-		return ({char, factor, sum, sumFin}, i) => console.log(`    `, char, factor, sum, sumFin, i);
+	return ({factor, sum, digitIndex}) => {
+		console.log(name, factor, sum, null == digitIndex ? `` : `d${digitIndex}`);
+		return ({charLast, charCount, factor, sum, sumFin, digitIndex}) =>
+			console.log(`    `, charLast, factor, sum, sumFin, `${null == digitIndex ? `` : `d${digitIndex}.`}${charCount}`);
+			// [digitAt, posicoes, soma, finalize(soma), index]
 	};
 }
 
 export function getDigitsCPFManual(valor) {
 	valor = String(valor).replace(/[^0-9]/g, '');
 	var digitos = valor.substr(0, 9);
-	const fnDebugItem = undefined;// fnFnDebugItem(`getDigitsCPFManual debugItem`);
-	var dv1 = calcDigitSum({factor: 10, fnDebugItem}).addText(digitos).finalize();
-	var dv2 = calcDigitSum({factor: 11, fnDebugItem}).addText(digitos + dv1).finalize();
+	const fnDebugItem = undefined; // fnFnDebugItem(`getDigitsCPFManual debugItem`); // 
+	var dv1 = calcDigitSum({factor: 10, digitIndex: 0, fnDebugItem}).addText(digitos).finalize();
+	var dv2 = calcDigitSum({factor: 11, digitIndex: 1, fnDebugItem}).addText(digitos + dv1).finalize();
 	return `${dv1}${dv2}`;
 }
 
 export function getDigitsCPF(digits) {
 	digits = String(digits).replace(/[^0-9]/g, '').substr(0, 9);
-	const fnDebugItem = undefined;// fnFnDebugItem(`getDigitsCPF debugItem`);
-	return calcDigitCPF().setDebug(fnDebugItem).addText(digits).finalize().join('');
+	const fnDebugItem = undefined; // fnFnDebugItem(`getDigitsCPF debugItem`); // 
+	return calcDigitCPF(fnDebugItem).addText(digits).finalize().join('');
 }
 
 export function checkCPF(valor) {
@@ -144,16 +152,16 @@ export function checkCPF(valor) {
 export function getDigitsCNPJManual(valor) {
 	valor = String(valor).replace(/[^0-9]/g, '');
 	var digitos = valor.substr(0, 12);
-	const fnDebugItem = undefined;// fnFnDebugItem(`getDigitsCNPJManual debugItem`);
-	var dv1 = calcDigitSum({factor: 5, fnDebugItem}).addText(digitos).finalize();
-	var dv2 = calcDigitSum({factor: 6, fnDebugItem}).addText(digitos + dv1).finalize();
+	const fnDebugItem = undefined; // fnFnDebugItem(`getDigitsCNPJManual debugItem`); // 
+	var dv1 = calcDigitSum({factor: 5, digitIndex: 0, fnDebugItem}).addText(digitos).finalize();
+	var dv2 = calcDigitSum({factor: 6, digitIndex: 1, fnDebugItem}).addText(digitos + dv1).finalize();
 	return `${dv1}${dv2}`;
 }
 
 export function getDigitsCNPJ(digits) {
 	digits = String(digits).replace(/[^0-9]/g, '').substr(0, 12);
-	const fnDebugItem = undefined;// fnFnDebugItem(`getDigitsCNPJ debugItem`);
-	return calcDigitCNPJ().setDebug(fnDebugItem).addText(digits).finalize().join('');
+	const fnDebugItem = undefined; // fnFnDebugItem(`getDigitsCNPJ debugItem`); // 
+	return calcDigitCNPJ(fnDebugItem).addText(digits).finalize().join('');
 }
 
 export function checkCNPJ(valor) {
