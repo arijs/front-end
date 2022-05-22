f09f730e74e44cc2017a3e52f2410b05bd1a5f93
Rafael Hengles <rhengles@gmail.com>
2022-05-22 20:04:46 -0300

    Fix utils/{pages, pagesCurrent}

--- src/isomorphic/utils/pages.mjs
+++ src/isomorphic/utils/pages.mjs
@@ -1,7 +1,7 @@
 
-export function pages(n, steps) {
+export function pages(n, steps, offset = 0) {
 	if (!(steps instanceof Array)) steps = [1, 2, 5, 10];
-	var m = (n - 1) * 0.5;
+	var m = n * 0.5;
 	var mult = 1;
 	var d = [];
 	var si = 0;
@@ -14,7 +14,7 @@ export function pages(n, steps) {
 			sl = p;
 			break;
 		}
-		d.push(p);
+		d.push(p + offset);
 		si += 1;
 		if (si == sc) {
 			mult *= steps[si-1];
@@ -23,14 +23,17 @@ export function pages(n, steps) {
 	}
 	var dr = [];
 	for (si = 0, sc = d.length; si < sc; si++) {
-		dr.unshift(n - d[si]);
+		dr.unshift((n + 1) - (d[si] - offset) + offset);
 	}
-	var dm = sl > 0 && sl < n && (!dr.length || sl < dr[0]) ? [sl] : [];
+	var dm = sl > 0 && sl < (n + 1) && (!dr.length || (sl + offset) < dr[0]) ? [sl + offset] : [];
+	// console.log(`Pages`, { n, steps, offset, d, dm, dr })
 	return d.concat(dm, dr);
 }
 
 export function pagesCurrent(current, total, steps) {
-	return pages(current, steps, 0)
+	return current < 1 || total < current
+		? pages(total, steps, 0)
+		: pages(current - 1, steps, 0)
 		.concat([current])
-		.concat(pages(total - current + 1, steps, current));
+		.concat(pages(total - current, steps, current));
 }
